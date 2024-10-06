97708d91e4f085bd8eba58e854f4348aed5d8a48
George Carr <gwcgreen3@gmail.com>
2024-10-06 22:37:03 +0100

    Email Basic

---
+++ Client/Models/EmailRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace PersonalStaticApp.Client.Models
+{
+    public class EmailRequest
+    {
+        public string? To { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+
+    }
+}
